f3cfed563e753a76a3bc12f36e53f66288bac467
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2024-03-26 17:03:54 -0300

    26/03

--- s_a_escola/src/main/java/webapp/s_a_escola/Controller/AdministradorController.java
+++ s_a_escola/src/main/java/webapp/s_a_escola/Controller/AdministradorController.java
@@ -2,27 +2,30 @@ package webapp.s_a_escola.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import webapp.s_a_escola.Model.AdministradorHabilitado;
+import org.springframework.web.bind.annotation.PostMapping;
+import webapp.s_a_escola.Model.Administrador;
 import webapp.s_a_escola.Repository.AdministradorRepository;
+import webapp.s_a_escola.Repository.PreCadRepository;
+
 
 @Controller
 public class AdministradorController {
     @Autowired
     private AdministradorRepository ar;
+    
+    @Autowired
+    private PreCadRepository pcar;
 
-    @RequestMapping(value = "/administrador", method = RequestMethod.GET)
-    public String abrirAdministrador(Model model) {
-        model.addAttribute("administrador", new AdministradorHabilitado());
-        return "administrador/administrador";
+    @PostMapping("cadastro-adm")
+    public String postCadastroAdm(Administrador adm) {
+        ar.save(adm);
+        
+        if (pcar.findByCpf(adm.getCpf()).equals(adm)) {
+            System.out.println("Cadastro realizado com sucesso");            
+        }
+        
+        return "login/login-adm";
     }
+    
 
-    @RequestMapping(value = "/administrador", method = RequestMethod.POST)
-    public String gravarAdministrador(@ModelAttribute("administrador") AdministradorHabilitado administrador) {
-        ar.save(administrador);
-        return "redirect:/administrador?success=true";
-    }
 }
--- s_a_escola/src/main/java/webapp/s_a_escola/Repository/AdministradorRepository.java
+++ s_a_escola/src/main/java/webapp/s_a_escola/Repository/AdministradorRepository.java
@@ -1,12 +1,8 @@
 package webapp.s_a_escola.Repository;
 
-import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import webapp.s_a_escola.Model.AdministradorHabilitado;
+import webapp.s_a_escola.Model.Administrador;
 
-public interface AdministradorRepository extends CrudRepository<AdministradorHabilitado, Long> {
-    // criado para a busca Administrador por id ou chave primária
-    @Query(value = "select u from administradores_habilitados u where u.cpf like %?1%")
-    List<AdministradorHabilitado> findByCPF(long cpf);
+public interface AdministradorRepository extends CrudRepository<Administrador, String>{
+        
 }
